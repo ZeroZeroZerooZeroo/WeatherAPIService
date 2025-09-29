0ad522dc4449764d2d8ba8ec7b2d12cbf1e6d343
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-09-29 19:07:01 +0700

    Добавлен метод GetWeather для запроса погоды по городу. Реализована обработка HTTP ошибок и статус-кодов API.Добавлен парсинг JSON ответа в структуру данных. Выполнено преобразование полного ответа API в упрощенную модель. Добавлена обработка ошибок на всех этапах запроса

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 
@@ -23,7 +25,34 @@ func NewWeatherService(httpClient *http.Client, apyKey, baseURL string) *Weather
 }
 
 func (ws *WeatherService) GetWeather(city string) (*models.WeatherResponse, error) {
+	url := ws.buildURL(city)
+	resp, err := ws.httpClient.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка запроса:%w", err)
+	}
+	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("API ошибка: %s, тело: %s", resp.Status, string(body))
+
+	}
+
+	var apiResponse models.WeatherAPIResponse
+	if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
+		return nil, fmt.Errorf("ошибка парсинга JSON: %w", err)
+
+	}
+	if len(apiResponse.Weather) == 0 {
+		return nil, fmt.Errorf("нет данных о погоде")
+	}
+
+	weather := &models.WeatherResponse{
+		City:        apiResponse.Name,
+		Temperature: apiResponse.Main.Temp,
+		Description: apiResponse.Weather[0].Description,
+	}
+	return weather, nil
 }
 
 func (ws *WeatherService) buildURL(city string) string {
