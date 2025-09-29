4072a354b1c7a75a3e0da766b79df2fc4bae8425
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-09-29 18:50:13 +0700

    Добавлена структура WeatherAPIResponse для парсинга ответа от внешнего API.  Добавлена структура WeatherResponse для формирования ответа пользователю.

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,17 @@
 package models
+
+type WeatherAPIResponse struct {
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+	Name string `json:"name"`
+}
+
+type WeatherResponse struct {
+	City        string  `json:"city"`
+	Temperature float64 `json:"temperature"`
+	Description string  `json:"description"`
+}
