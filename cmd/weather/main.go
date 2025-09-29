7860c1275ab9d06435a875348a45b586a56a5d8d
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-09-29 19:18:43 +0700

    Создан WeatherHandler для обработки HTTP запросов. Реализован метод GetWeather для обработки GET /weather?city=... Добавлена валидация обязательного параметра city. Интегрирован WeatherService для получения данных о погоде. Реализована корректная обработка ошибок и JSON кодирование. Настроены соответствующие HTTP статус-коды для разных сценариев.

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/ZeroZeroZerooZeroo/WeatherAPIService/internal/config"
+	"github.com/ZeroZeroZerooZeroo/WeatherAPIService/internal/handler"
+	"github.com/ZeroZeroZerooZeroo/WeatherAPIService/internal/service"
 )
 
 func main() {
@@ -12,6 +15,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
 
+	}
+
+	WeatherService := service.NewWeatherService(
+		&http.Client{},
+		cfg.ApiKey,
+		cfg.ApiURL,
+	)
+
+	weatherHandler := handler.NewHandler(WeatherService)
+
+	http.HandleFunc("/weather", weatherHandler.GetWeather)
+
+	log.Printf("Сервер запущен на порту %s", cfg.ServerPort)
+
+	if err := http.ListenAndServe(":"+cfg.ServerPort, nil); err != nil {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
 	}
 
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ZeroZeroZerooZeroo/WeatherAPIService/internal/service"
+)
+
+type WeatherHandler struct {
+	weatherService *service.WeatherService
+}
+
+func NewHandler(ws *service.WeatherService) *WeatherHandler {
+	return &WeatherHandler{
+		weatherService: ws,
+	}
+}
+
+func (h *WeatherHandler) GetWeather(w http.ResponseWriter, r *http.Request) {
+	city := r.URL.Query().Get("city")
+	if city == "" {
+		http.Error(w, "Параметр 'city' обязателен", http.StatusBadRequest)
+		return
+	}
+
+	data, err := h.weatherService.GetWeather(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Ошибка кодирования ответа", http.StatusInternalServerError)
+	}
+}
